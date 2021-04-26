98c4c8d67df57da6c8628afb1b0f09b692ec9b27
Sabadeyan <82463034+Sabadeyan@users.noreply.github.com>
2021-04-26 14:49:11 +0400

    Add files via upload

---
+++ Hom1.py
@@ -0,0 +1,19 @@
+#
+# def char_count(input_string,input_char):
+#     count=0
+#     for i in input_string:
+#         if input_char ==i:
+#             count+=1
+#     return count
+# print(char_count("hambal","a"))
+
+
+
+
+def tareri_qanak(mutqagrvatc_text,mutqagrvats_tar):
+    qanak=0
+    for tar in mutqagrvatc_text:
+        if tar == mutqagrvats_tar:
+           qanak+=1
+    return qanak
+print(tareri_qanak("BararanBBBBB", "B"))
